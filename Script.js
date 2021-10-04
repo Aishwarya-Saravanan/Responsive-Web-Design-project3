bef1437b304b26a90f6af0188ea5e27f46afa951
Aishwarya-Saravanan <88574757+Aishwarya-Saravanan@users.noreply.github.com>
2021-10-04 16:05:03 +0530

    Add files via upload

---
+++ Script.js
@@ -0,0 +1,5 @@
+$(document).ready(function(){    /* Document is uploaded */
+    $('.burger-menu').on('click',function(){   /* click on the burger-menu function */
+   $('.mob-nav').toggle();  /* Toggle the mobile-nav */
+    })   
+   });
